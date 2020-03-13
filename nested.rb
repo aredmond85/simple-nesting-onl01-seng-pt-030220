34a7b47fbf08b65f2c5a2d6db493f63b50bcc5a1
Austin Redmond <aredmond85@gmail.com>
2020-03-13 00:14:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
         }
      }
 		 programmer_hash [:dennis_ritchie][:languages] << "Assembly"
-		 return programmer_hash [:dennis_ritchie][:languages]
+		 return programmer_hash
 
 end
